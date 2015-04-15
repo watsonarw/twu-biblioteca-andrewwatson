5b73f28162cbbadcec295fe773cace920ea7f1c6
Andrew Watson <watson.arw@gmail.com>
2015-04-16 00:59:31 +1000

    Test for welcome message (failing)

--- test/com/twu/biblioteca/ExampleTest.java
+++ test/com/twu/biblioteca/ExampleTest.java
@@ -1,13 +1,32 @@
 package com.twu.biblioteca;
 
 
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
 import static org.junit.Assert.assertEquals;
 
 public class ExampleTest {
 
+    private ByteArrayOutputStream outStream = new ByteArrayOutputStream();
+
+    @Before
+    public void setupStreams() {
+        System.setOut(new PrintStream(outStream));
+    }
+
+    @After
+    public void tearDown() {
+        System.setOut(null);
+    }
+
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void testWelcomeMessage() {
+        String expected = "Welcome to Biblioteca.\n";
+        BibliotecaApp.main(null);
+        assertEquals(expected, outStream.toString());
     }
 }
