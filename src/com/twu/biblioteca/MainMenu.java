69447e89b423b6c239cef9cca272ea6036943c06
Andrew Watson <watson.arw@gmail.com>
2015-04-17 00:31:00 +1000

    Implement MainMenu handleResponse method to show list of books

--- src/com/twu/biblioteca/MainMenu.java
+++ src/com/twu/biblioteca/MainMenu.java
@@ -1,5 +1,9 @@
 package com.twu.biblioteca;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 /**
  * Created by watsonarw on 16/04/15.
  */
@@ -9,5 +13,15 @@ public class MainMenu {
         System.out.print("------ Main Menu ------\n" +
                            "Select an option below:\n" +
                            " 1 | List Books\n");
+    }
+
+    public void handleResponse() {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            int response = br.read();
+            System.out.print(BibliotecaApp.getLibrary().getBookList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
