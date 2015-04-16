ad21a97c211c67429693626242ed1953c44d190f
Andrew Watson <watson.arw@gmail.com>
2015-04-16 22:33:40 +1000

    Update LibraryTest for Author and Year (failing)

--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -12,16 +12,16 @@ public class LibraryTest {
     @Test
     public void testSingleBookLibrary() {
         Library library = new Library();
-        library.addBook("The Hobbit");
-        assertEquals("The Hobbit\n",library.getBookList());
+        library.addBook("The Hobbit", "J.R.R. Tolkien", 1937);
+        assertEquals("The Hobbit - J.R.R. Tolkien, 1937\n",library.getBookList());
     }
 
 
     @Test
     public void testMultiBookLibrary() {
         Library library = new Library();
-        library.addBook("The Hobbit");
-        library.addBook("Catcher in the Rye");
-        assertEquals("The Hobbit\nCatcher in the Rye\n",library.getBookList());
+        library.addBook("The Hobbit", "J.R.R. Tolkien", 1937);
+        library.addBook("Catcher in the Rye", "J.D. Salinger", 1951);
+        assertEquals("The Hobbit - J.R.R. Tolkien, 1937\nCatcher in the Rye - J.D. Salinger, 1951\n",library.getBookList());
     }
 }
