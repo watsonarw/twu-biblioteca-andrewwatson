d1d11be2c13980d02e027d5a01945fc9850078e9
Andrew Watson <watson.arw@gmail.com>
2015-04-16 23:41:56 +1000

    Implement MainMenu class with show method

---
+++ src/com/twu/biblioteca/MainMenu.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+/**
+ * Created by watsonarw on 16/04/15.
+ */
+public class MainMenu {
+
+    public void show() {
+        System.out.print("------ Main Menu ------\n" +
+                           "Select an option below:\n" +
+                           " 1 | List Books\n");
+    }
+}
