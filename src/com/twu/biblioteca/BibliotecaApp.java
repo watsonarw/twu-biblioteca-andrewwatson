b5e4942531f493d0b0d6deba24b312f360b70654
Andrew Watson <watson.arw@gmail.com>
2015-04-16 20:39:52 +1000

    Basic implementation of listing books

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -4,9 +4,19 @@ public class BibliotecaApp {
 
     public static void main(String[] args) {
         displayWelcomeMessage();
+        displayListOfBooks();
     }
 
     private static void displayWelcomeMessage() {
         System.out.println("Welcome to Biblioteca.");
+    }
+
+    private static void displayListOfBooks() {
+        System.out.println( "The Lord of The Rings\n" +
+                            "To Kill a Mockingbird\n" +
+                            "Nineteen Eighty Four\n" +
+                            "Pride and Prejudice\n" +
+                            "His Dark Materials"
+                        );
     }
 }
